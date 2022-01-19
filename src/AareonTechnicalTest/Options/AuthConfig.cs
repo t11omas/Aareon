991118f82199160d0ab5e60983ffa519d2aca304
Thomas Anderson <thomas.anderson@oscore.io>
2022-01-19 09:08:14 +0000

    Add simple authentication

---
+++ src/AareonTechnicalTest/Options/AuthConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AareonTechnicalTest.Options
+{
+    public class AuthConfig
+    {
+        public const string Name = nameof(AuthConfig);
+
+        [Required]
+        public string Secret { get; set; }
+
+        [Required]
+        public string Issuer { get; set; }
+
+        [Required]
+        public string Audience { get; set; }
+
+        public int AccessTokenExpiration { get; set; } = 10;
+    }
+}
