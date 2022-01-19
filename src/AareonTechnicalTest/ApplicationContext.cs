95bfc220ce2ca1d81a58e0362348ad5c9530a6c6
Thomas Anderson <thomas.anderson@oscore.io>
2022-01-19 09:26:03 +0000

    Add Auditing

--- src/AareonTechnicalTest/ApplicationContext.cs
+++ src/AareonTechnicalTest/ApplicationContext.cs
@@ -1,6 +1,9 @@
 ﻿using AareonTechnicalTest.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Z.EntityFramework.Plus;
 
 namespace AareonTechnicalTest
 {
@@ -16,6 +19,25 @@ namespace AareonTechnicalTest
         public virtual DbSet<Ticket> Tickets { get; set; }
 
         public virtual DbSet<Note> Notes { get; set; }
+
+        public virtual DbSet<AuditEntry> AuditEntries { get; set; }
+        public virtual DbSet<AuditEntryProperty> AuditEntryProperties { get; set; }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            var audit = new Audit();
+            audit.PreSaveChanges(this);
+            var rowAffecteds = await base.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+            audit.PostSaveChanges();
+
+            if (audit.Configuration.AutoSavePreAction != null)
+            {
+                audit.Configuration.AutoSavePreAction(this, audit);
+                await base.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+            }
+
+            return rowAffecteds;
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
